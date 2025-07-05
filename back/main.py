badac2da06794628e119b454c062f7b8b4b89441
Braz Souza <brazsdsgabriel@gmail.com>
2025-07-05 18:44:58 -0300

    feat: add Dockerfile

--- back/main.py
+++ back/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-from app.routers.questoes import questoes_router
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/avaliar-back",
+    docs_url="/docs",
+    openapi_url="/openapi.json",
+    title="Avaliar - API",
+    description="A API para a plataforma de produção e avaliação de provas Avaliar",
+    version="0.0.0",
+    openapi_version="3.1.0"
+)
 
 @app.get("/health", tags=["health"])
 async def health():
@@ -9,4 +16,4 @@ async def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8001, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
