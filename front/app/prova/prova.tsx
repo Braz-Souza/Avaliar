9101f2757ff5a3533984a6b071f85243e690d53a
bz <mail@braz.one>
2025-09-21 17:25:12 -0300

    feat: add pdf render in prova page

--- front/app/prova/prova.tsx
+++ front/app/prova/prova.tsx
@@ -13,16 +13,33 @@ export function meta({}: Route.MetaArgs) {
 export default function Prova() {
   return (
     <main className="flex items-center justify-center h-screen bg-base-200">
-      <NavLink
-        className="absolute top-0 left-0 m-4 btn btn-primary gap-4"
-        to="/"
-      >
-        <ArrowLeft />
-        Voltar
-      </NavLink>
-      { /* Sidebar de baixar latex, pdf */}
-      { /* Input de latex */}
-      { /* Output de latex */}
+      <div className="flex flex-col max-w-36">
+        <NavLink
+          className="btn btn-primary gap-4 flex-1 p-2 m-2"
+          to="/"
+        >
+          <ArrowLeft />
+          Voltar
+        </NavLink>
+        <button
+          className="btn btn-primary btn-outline flex-1 p-2 m-2"
+          >
+          Baixar PDF
+        </button>
+        <button
+          className="btn btn-primary btn-outline flex-1 p-2 m-2"
+          >
+          Baixar Latex
+        </button>
+      </div>
+      <div className="h-full w-fit bg-base-400 flex flex-1 p-8 gap-8">
+        <textarea className="bg-base-100 shadow textarea flex flex-1 h-full" placeholder="Insert LaTeX here"></textarea>
+        <div className="bg-base-100 shadow textarea flex flex-1 h-full w-full">
+          <object data="./sample.pdf" type="application/pdf" className="w-full">
+              <div>No PDF available</div>
+          </object>
+        </div>
+      </div>
     </main>
   );
 }
