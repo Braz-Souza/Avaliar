693efc5a6971ccc74596e8d20517217b1c951f34
Braz Souza <mail@braz.one>
2025-11-07 16:57:41 -0300

    style: remove unecessary help text

--- front/app/features/prova/components/Sidebar.tsx
+++ front/app/features/prova/components/Sidebar.tsx
@@ -143,24 +143,6 @@ export function Sidebar({
             </button>
           </div>
         </div>
-
-        {/* Help Section */}
-        <div className="pt-4">
-          <div className="alert alert-info shadow-lg">
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
-              <div className="text-xs">
-                <p className="font-semibold mb-1">Formato de Questões:</p>
-                <p className="opacity-80">Q: questão simples</p>
-                <p className="opacity-80">QM: múltipla escolha</p>
-                <p className="opacity-80">a), b), c)... opções</p>
-                <p className="opacity-80">* marca resposta correta</p>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
 
       {/* Footer Info */}
